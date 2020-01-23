f10c14de1221ca83b0465c9c04dd8251bc0f327e
Erivks <mordecai.es27@gmail.com>
2020-01-23 16:29:54 -0300

    Montando tabela

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,7 +1,33 @@
 @extends('layouts.app')
 
 @section('body')
-    @foreach ($categories as $category)
-    <p>{{ $category['nome'] }}</p>
-    @endforeach
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Categorias</h5>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">
+                            ID
+                        </th>
+                        <th scope="col">
+                            Nome
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($categories as $category)
+                        <tr>
+                            <td>
+                                {{ $category['id'] }}
+                            </td>
+                            <td>
+                                {{ $category['nome'] }}
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>    
 @endsection
