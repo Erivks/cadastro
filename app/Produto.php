29ef0ce54e06053bb13832e318273d2078b3a2d4
Erivks <mordecai.es27@gmail.com>
2020-01-30 13:53:37 -0300

    Desenvolvendo model

--- app/Produto.php
+++ app/Produto.php
@@ -10,4 +10,35 @@ class Produto extends Model
     {
         return self::all();
     }
+
+    public static function storeProduct($request)
+    {
+        $product = new Produto();
+        $product->nome = $request->input('productName');
+        $product->estoque = $request->input('productStock');
+        $product->preco = $request->input('productPrice');
+        $product->categoria_id = $request->input('productCategory');
+        $product->save();
+    }
+
+    public static function getProduct($id)
+    {
+        return self::find($id);
+    }
+
+    public static function updateProduct($request, $id)
+    {
+        Produto::where('id', $id)
+            ->update([
+                        'nome' => $request->input('productName'),
+                        'estoque' => $request->input('productStock'),
+                        'preco' => $request->input('productPrice'),
+                        'categoria_id' => $request->input('productCategory')
+                    ]);
+    }
+
+    public static function deleteProduct($id)
+    {
+        self::destroy($id);
+    }
 }
