190f8866a96bb16a5e4ea629c72d5988a04006c2
Erivks <mordecai.es27@gmail.com>
2020-02-03 14:06:22 -0300

    Refazendo função

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,9 +29,9 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
         <script>
-            $('.deleteButtonProduct').on('click', function() {
-                let idProduct = $(this).data('id');
-                let routeDelete = '/produtos/deletar/' + idProduct;
+            $('.deleteButtonCategory').on('click', function() {
+                let idCategory = $(this).data('id');
+                let routeDelete = '/categorias/deletar/' + idCategory;
                 $('#deleteButton').attr('href', routeDelete);
             });
         </script>
