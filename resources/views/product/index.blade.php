1eb19a5915e50960a1a95b6c8102dc27c40fa977
Erivks <mordecai.es27@gmail.com>
2020-02-04 15:01:16 -0300

    Montando estrutura para uso de Ajax

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -2,92 +2,46 @@
 
 @section('body')
 <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">Produtos</h5>
-                @if (count($products) > 0)
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">
-                                    ID
-                                </th>
-                                <th scope="col">
-                                    Nome
-                                </th>
-                                <th scope="col">
-                                    Estoque
-                                </th>
-                                <th scope="col">
-                                    Preço
-                                </th>
-                                <th scope="col">
-                                    Categoria
-                                </th>
-                                <th scope="col">
-                                    Editar
-                                </th>
-                                <th scope="col">
-                                    Deletar
-                                </th>
-                                <th scope="col">
-                                    Detalhe
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($products as $product)
-                                <tr>
-                                    <td>
-                                        {{ $product['id'] }}
-                                    </td>
-                                    <td>
-                                        {{ $product['nome'] }}
-                                    </td>
-                                    <td>
-                                        {{ $product['estoque'] }}
-                                    </td>
-                                    <td>
-                                        {{ $product['preco'] }}
-                                    </td>
-                                    <td>
-                                        {{ $product['cat_nome'] }}
-                                    </td>
-                                    <td>
-                                        <a href="{{ route('product.edit', $product['id']) }}" 
-                                            class="btn btn-success" 
-                                            role="button">
-                                            Editar
-                                        </a>
-                                    </td>
-                                    <td>
-                                        <a href="{{ route('product.delete', $product['id']) }}" 
-                                        class="btn btn-danger" role="button">
-                                            Deletar
-                                        </a>
-                                    </td>
-                                    <td>
-                                        <a href="#productDetails" class="btn btn-warning" 
-                                            aria-controls="collapseDetails" aria-expanded="false" 
-                                            data-toggle="collapse" role="button">
-                                            Detalhes
-                                        </a>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                @else
-                    <div class="alert alert-danger" role="alert">
-                        <p class="alert-text">
-                            Não existem registros de produtos cadastrados.
-                        </p>
-                    </div>
-                    <a href="{{ route('product.new') }}"
-                        class="btn btn-primary"
-                        role="button">
-                        Adicionar produto
-                    </a>
-                @endif
+    <div class="card-body">
+        <h5 class="card-title">Produtos</h5>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">
+                            ID
+                        </th>
+                        <th scope="col">
+                            Nome
+                        </th>
+                        <th scope="col">
+                            Estoque
+                        </th>
+                        <th scope="col">
+                            Preço
+                        </th>
+                        <th scope="col">
+                            Categoria
+                        </th>
+                        <th scope="col">
+                            Editar
+                        </th>
+                        <th scope="col">
+                            Deletar
+                        </th>
+                        <th scope="col">
+                            Detalhe
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                        
+                </tbody>
+            </table>
+        </div>
+        <div class="card-footer">
+            <button class="btn btn-primary" type="button">
+                Adicionar categoria
+            </button>
         </div>
     </div>    
 @endsection
